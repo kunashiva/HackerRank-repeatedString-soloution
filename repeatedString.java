29e26a078fbf67361b97b6f90f94bf90ee1ffbf6
Shiva Prasad Kuna <49793833+kunashiva@users.noreply.github.com>
2020-04-13 18:00:35 +0600

    Add files via upload

---
+++ repeatedString.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class repeatedString {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+       long count=0;
+       long totalcount=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='a')
+            {
+                count++;
+            }
+        }
+        long div=n/s.length();
+        totalcount=count*div;
+        long rem=n%s.length();
+        for(int i=0;i<rem;i++)
+        {
+            if(s.charAt(i)=='a')
+            {
+                totalcount++;
+            }
+        }
+
+        return totalcount;
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
+
